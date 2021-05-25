7a8b1caa1c13cf2a30b7048517e177ca43f96ba0
paullogoncalves <paullogoncalves@gmail.com>
2021-05-25 09:35:57 -0300

    Starting

---
+++ src/test/java/com/oragentalent5/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oragentalent5.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

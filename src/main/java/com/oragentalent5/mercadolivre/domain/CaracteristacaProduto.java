3b0d3d5c0e7445897a538994a3c64caa4ffe07a0
paullogoncalves <paullogoncalves@gmail.com>
2021-05-27 08:13:10 -0300

    usuario-logado-cadastra-produto

--- src/main/java/com/oragentalent5/mercadolivre/domain/CaracteristacaProduto.java
+++ src/main/java/com/oragentalent5/mercadolivre/domain/CaracteristacaProduto.java
@@ -1,13 +1,20 @@
 package com.oragentalent5.mercadolivre.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-
+@Entity
 public class CaracteristacaProduto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@NotBlank
 	private String nome;
@@ -15,6 +22,8 @@ public class CaracteristacaProduto {
 	@NotBlank
 	private String descricao;
 
+	@NotNull 
+	@Valid
 	@ManyToOne
 	private Produto produto;
 
@@ -29,28 +38,43 @@ public class CaracteristacaProduto {
 		this.produto = produto;
 	}
 
-	public String getNome() {
-		return nome;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		return result;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CaracteristacaProduto other = (CaracteristacaProduto) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		return true;
 	}
 
-	public Produto getProduto() {
-		return produto;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-
+	
+	
 }
